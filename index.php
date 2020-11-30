d63d40113e142686aea2257ee9ec9a0023df0ba7
Julestblt <70810590+Julestblt@users.noreply.github.com>
2020-11-30 12:50:13 +0100

    ex0done

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ function my_merge_image ($first_img_path, $second_img_path){
     $height = 256;
 
     //header("Content-Type: image/png");
-    $im = imagecreate($width, $height) or die("Cannot blabla");
+    $im = imagecreate($width, $height) or die("Error");
     $canvas = imagecolorallocate($im, 245, 245, 245);
     $image_a = imagecreatefrompng($first_img_path);
     imagecopyresampled($im, $image_a, 0, 0, 0, 0, 256, 256, 256, 256);
@@ -12,8 +12,7 @@ function my_merge_image ($first_img_path, $second_img_path){
     $image_a = imagecreatefrompng($second_img_path);
     imagecopyresampled($im, $image_a, 256, 0, 0, 0, 256, 256, 256, 256);
 
-    imagepng($im, "toto.png");
+    imagepng($im, "merge.png");
 
 }
-
 my_merge_image("a.png", "b.png");
