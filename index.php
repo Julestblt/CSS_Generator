a43691f14eaa0c4ae0ef7c07e2947d49ce05c850
Julestblt <70810590+Julestblt@users.noreply.github.com>
2020-11-30 12:29:59 +0100

    ex0done

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+function my_merge_image ($first_img_path, $second_img_path){
+    $width = 512;
+    $height = 256;
+
+    //header("Content-Type: image/png");
+    $im = imagecreate($width, $height) or die("Cannot blabla");
+    $canvas = imagecolorallocate($im, 245, 245, 245);
+    $image_a = imagecreatefrompng($first_img_path);
+    imagecopyresampled($im, $image_a, 0, 0, 0, 0, 256, 256, 256, 256);
+
+    $image_a = imagecreatefrompng($second_img_path);
+    imagecopyresampled($im, $image_a, 256, 0, 0, 0, 256, 256, 256, 256);
+
+    imagepng($im, "toto.png");
+
+}
+
+my_merge_image("a.png", "b.png");
