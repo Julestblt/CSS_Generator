b6c4aea57162c4885245d192adf7006fb0485e77
Julestblt <70810590+Julestblt@users.noreply.github.com>
2020-12-02 17:07:55 +0100

    DAY03

--- index.php
+++ index.php
@@ -18,29 +18,38 @@ function my_merge_image($first_img_path, $second_img_path, $filename){
 
     #Création du fichier css et transfert des données dans le fichier.
         $fp = fopen("style.css", 'w');
-        fwrite($fp, ".sprite {\n background-image: url($filename); \n}\n");
-        fwrite($fp, ".img1 {\n width: ".$img1_width."px;\n height: ".$img1_height."px;\n}");
-        fwrite($fp, "\n.img2 {\n width: ".$img2_width."px;\n height: ".$img2_height."px;\n}");
+        fwrite($fp, ".sprite {
+         background-image: url($filename);
+         background-repeat: no-repeat;
+         display: block;
+         }\n");
+
+        fwrite($fp, ".img1 {
+         width: ".$img1_width."px;
+         height: ".$img1_height."px;
+         background-position: 0px 0px;
+         }\n");
+
+        fwrite($fp, ".img2 {
+         width: ".$img2_width."px;
+         height: ".$img2_height."px;
+         background-position: 0px ".$img1_height."px;
+         }");
         fclose($fp);
 
-}
-my_merge_image("a.png", "b.png", "merge.png");
+        if ($handle = opendir('.')) {
 
+            while (false !== ($entry = readdir($handle))) {
 
+                if ($entry != "." && $entry != ".." && substr($entry, -3) == "png") {
 
-#ETAPE 2
-/*function my_scandir($dir_path){
-
-    if ($handle = opendir('.')) {
-
-        while (false !== ($entry = readdir($handle))) {
-
-            if ($entry != "." && $entry != ".." && substr($entry, -3) == "png") {
-
-                echo "$entry\n";
+                    echo "$entry\n";
+                }
             }
+
+            closedir($handle);
         }
 
-        closedir($handle);
-    }
+
 }
+my_merge_image("c.png", "d.png", "merge.png");
